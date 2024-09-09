1d72ddfee01ef06b3a2170f61a73578504c37902
Mehedi Hasan <mehedihasan67705251@gmail.com>
2024-09-09 22:17:31 +0600

    again build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
+
 import MainLayout from "./components/layout/MainLayout";
-
-
 
 function App() {
   return (
--- src/components/layout/Mainlayout.tsx
+++ src/components/layout/Mainlayout.tsx
@@ -7,7 +7,6 @@ const MainLayout = () => {
     <div>
       <Navbar />
       <Outlet />
-
       <Footer />
     </div>
   );
