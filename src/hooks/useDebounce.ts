3e96434910b58331d801612633242e20e74b40fd
Mehedi <mehedihasan67705251@gmail.com>
2025-10-19 21:32:12 +0600

    add custom hook useDebounce for debouncing function calls

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useCallback } from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useDebounce<T extends (...args: any[]) => any>(
+  callback: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+
+      timeoutRef.current = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    },
+    [callback, delay]
+  );
+}
